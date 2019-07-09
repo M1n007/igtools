346f97a3e8586095bf5d7d98f8b1b1bf2f211c41
Amin <amin4udin@gmail.com>
2019-07-09 10:49:09 +0700

    update cookie

--- autoFollowers.js
+++ autoFollowers.js
@@ -28,15 +28,11 @@ const genSes = length =>
     });
 
 (async () => {
-
-    const csrf = await genSes(32);
-    const mid = await genSes(28);
-
     const newData = [];
 
 
     await connection.query(
-        `SELECT * FROM user_not_checkpoint`,
+        `SELECT * FROM ray_user`,
         function (error, results, fields) {
             if (error) return 'error';
             delay(5000)
@@ -49,27 +45,49 @@ const genSes = length =>
     await delay(5000);
     for (let index = 0; index < newData.length; index++) {
         const element = newData[index];
+        // const element = {
+        //     username: 'amin4udin',
+        //     password: 'berak321kiky'
+        // }
         await delay(2000)
         const Cookie = await getCookie.functionGetCookie();
         const csrfToken = Cookie[7].split(';')[0];
         const rurz = Cookie[8].split(';')[0];
         const mid = Cookie[9].split(';')[0];
         const LoginToDO = await login.functionLogin(element.username, element.password, csrfToken, rurz, mid);
-        const getCookie = await login.functionGetCookie(element.username, element.password, csrfToken, rurz, mid);
-        const rur = getCookie.extensions.join().split(',')[1];
-        const ds = getCookie.extensions.join().split(',')[3];
-        const sessionId = getCookie.extensions.join().split(',')[5];
+        const getCookies = await login.functionGetCookie(element.username, element.password, csrfToken, rurz, mid);
+        console.log(getCookies.extensions)
         if (LoginToDO.authenticated === true) {
-            const post = {
-                username: element.username,
-                password: element.password,
-                account_id: element.account_id
+            if (getCookies.extensions.join().split(',')[9] !== undefined) {
+                const shbid = getCookie.extensions.join().split(',')[1];
+                const shbts = getCookie.extensions.join().split(',')[3];
+                const rur = getCookies.extensions.join().split(',')[5];
+                const ds = getCookies.extensions.join().split(',')[7];
+                const sessionId = getCookies.extensions.join().split(',')[9];
+                const post = {
+                    username: element.username,
+                    password: element.password
+                }
+                await delay(1000);
+                const follow = await Follow.functionFollow(csrfToken, mid, ds, rur, sessionId, shbid, shbts);
+                if (follow.status === 'ok') {
+                    console.log(follow, post.username)
+                }
+            } else {
+                const rur = getCookies.extensions.join().split(',')[1];
+                const ds = getCookies.extensions.join().split(',')[3];
+                const sessionId = getCookies.extensions.join().split(',')[5];
+                const post = {
+                    username: element.username,
+                    password: element.password
+                }
+                await delay(1000);
+                const follow = await Follow.functionFollow(csrfToken, mid, ds, rur, sessionId);
+                if (follow.status === 'ok') {
+                    console.log(follow, post.username)
+                }
             }
-            await delay(1000);
-            const follow = await Follow.functionFollow(csrf, mid, ds, rur, sessionId);
-            if (follow.status === 'ok') {
-                console.log(follow, post.username)
-            }
+
 
 
         }
