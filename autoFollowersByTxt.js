33b55266e84239ae6c97f1ccf0a252e1559356fe
Amin <amin4udin@gmail.com>
2019-07-15 11:53:07 +0700

    adding user agent

--- autoFollowersByTxt.js
+++ autoFollowersByTxt.js
@@ -7,6 +7,7 @@ const getCookie = require('./lib/getCookie');
 const getUserId = require('./lib/getUSerId');
 const moment = require('moment');
 const colors = require("./lib/colors");
+const UA = require('./lib/utils/uaGen');
 const readlineSync = require('readline-sync');
 const fs = require("fs");
 require('dotenv').config();
